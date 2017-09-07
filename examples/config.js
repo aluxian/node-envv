2931ad2b8931ada3a402cce7bed0b2e6bbc44089
Alexandru Rosianu <me@aluxian.com>
2017-09-07 08:31:40 +0200

    Add 'use strict' header to files

--- examples/config.js
+++ examples/config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const envv = require('..');
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * @param {string} name the name of the env var to retrieve, as found in process.env
  * @param {any} defaultValue return this value if process.env[name] is undefined
