686c7d61f64e2ed8ea1fc5099912b2440907fa23
Alexandru Rosianu <me@aluxian.com>
2018-03-02 11:54:00 +0000

    feat: add type cast

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 /**
  * @param {string} name the name of the env var to retrieve, as found in process.env
  * @param {any} [defaultValue] return this value if process.env[name] is undefined
+ * @param {any} [parseAsType] try to parse the value as this type before returning it
  * @return {any} the env value or defaultValue, whichever is defined
  * @throws if both the env value and defaultValue are undefined
  */
-function envv (name, defaultValue) {
+function envv (name, defaultValue = undefined, parseAsType = undefined) {
   if (!name) throw new Error(`envv() argument 'name' is required`);
   if (typeof name !== 'string') throw new TypeError(`envv() argument 'name' must be a string`);
   if (arguments.length > 2) throw new Error(`envv() expected at most 2 arguments`);
@@ -21,6 +22,20 @@ function envv (name, defaultValue) {
   // fallback to error
   if (value === undefined) {
     throw new Error(`env var '${name}' is missing`);
+  }
+
+  // try to parse
+  switch (parseAsType) {
+    case Boolean:
+      if (typeof value === 'string') {
+        value = value.toLowerCase();
+      }
+      value = JSON.parse(value);
+      break;
+
+    case String:
+      value = String(value);
+      break;
   }
 
   return value;
