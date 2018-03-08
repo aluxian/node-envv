49eb3007254ba2a15c480073324d39c5b219f880
Alexandru Rosianu <me@aluxian.com>
2018-03-08 11:27:02 +0000

    don't require extra arg for types, can be inferred from default

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 /**
  * @param {string} name the name of the env var to retrieve, as found in process.env
  * @param {any} [defaultValue] return this value if process.env[name] is undefined
- * @param {any} [parseAsType] try to parse the value as this type before returning it
  * @return {any} the env value or defaultValue, whichever is defined
  * @throws if both the env value and defaultValue are undefined
  */
-function envv (name, defaultValue = undefined, parseAsType = undefined) {
+function envv (name, defaultValue = undefined) {
   if (!name) throw new Error(`envv() argument 'name' is required`);
   if (typeof name !== 'string') throw new TypeError(`envv() argument 'name' must be a string`);
   if (arguments.length > 2) throw new Error(`envv() expected at most 2 arguments`);
 
+  // get value from process.env
   let value = process.env[name];
 
   // fallback to defaultValue
@@ -24,18 +24,17 @@ function envv (name, defaultValue = undefined, parseAsType = undefined) {
     throw new Error(`env var '${name}' is missing`);
   }
 
-  // try to parse
-  switch (parseAsType) {
-    case Boolean:
-      if (typeof value === 'string') {
-        value = value.toLowerCase();
-      }
-      value = JSON.parse(value);
-      break;
+  // try to parse as defaultValue type
+  if (typeof value === 'string') {
+    switch (typeof defaultValue) {
+      case 'boolean':
+        value = JSON.parse(value.toLowerCase());
+        break;
 
-    case String:
-      value = String(value);
-      break;
+      case 'number':
+        value = parseFloat(value);
+        break;
+    }
   }
 
   return value;
